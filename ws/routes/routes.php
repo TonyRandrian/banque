ec57df8c89afddd9617b6f00ac5fea41800e51b9
TonyRandrian <tony.randrian855@gmail.com>
2025-07-07 18:54:35 +0300

    p

--- ws/routes/routes.php
+++ ws/routes/routes.php
@@ -1,4 +1,3 @@
 <?php
-    require_once 'fond_routes.php';
-// Include all route files
-require_once __DIR__ . '/type_pret_routes.php';
+require_once 'fond_routes.php';
+require_once 'type_pret_routes.php';
