d9388fd1df12964a3312ffb734f5dde05e2d1506
TonyRandrian <tony.randrian855@gmail.com>
2025-07-07 16:09:26 +0300

    update sql

--- sql/script.sql
+++ sql/script.sql
@@ -1,119 +1,138 @@
-CREATE TABLE enum_pret
+CREATE TABLE enum_status_pret
 (
-    id      INT AUTO_INCREMENT PRIMARY KEY,
-    libelle VARCHAR(255) NOT NULL
+    id      INT AUTO_INCREMENT,
+    libelle VARCHAR(255) NOT NULL,
+    PRIMARY KEY (id)
+);
+
+CREATE TABLE type_pret
+(
+    id      INT AUTO_INCREMENT,
+    libelle VARCHAR(50)    NOT NULL,
+    taux    DECIMAL(15, 2) NOT NULL,
+    PRIMARY KEY (id)
 );
 
 CREATE TABLE modalite
 (
-    id      INT AUTO_INCREMENT PRIMARY KEY,
-    libelle VARCHAR(50), -- mensuel, trimestriel, semestriel
-    nb_mois INT NOT NULL
+    id      INT AUTO_INCREMENT,
+    libelle VARCHAR(100),
+    nb_mois INT NOT NULL,
+    PRIMARY KEY (id)
 );
 
-CREATE TABLE clients
+CREATE TABLE client
 (
-    id     INT AUTO_INCREMENT PRIMARY KEY,
+    id     INT AUTO_INCREMENT,
     email  VARCHAR(255) NOT NULL,
-    nom    VARCHAR(255) NOT NULL,
     prenom VARCHAR(255) NOT NULL,
-    mdp    VARCHAR(255) NOT NULL
+    mdp    VARCHAR(255) NOT NULL,
+    nom    VARCHAR(255) NOT NULL,
+    PRIMARY KEY (id)
 );
 
-CREATE TABLE employees
+CREATE TABLE employe
 (
-    id     INT AUTO_INCREMENT PRIMARY KEY,
+    id     INT AUTO_INCREMENT,
     nom    VARCHAR(255) NOT NULL,
     prenom VARCHAR(255) NOT NULL,
     email  VARCHAR(255) NOT NULL,
-    mdp    VARCHAR(255) NOT NULL
+    mdp    VARCHAR(255) NOT NULL,
+    PRIMARY KEY (id)
 );
 
 CREATE TABLE config
 (
-    id               INT AUTO_INCREMENT PRIMARY KEY,
+    id               INT AUTO_INCREMENT,
     libelle          VARCHAR(255) NOT NULL,
-    date_application DATE         NOT NULL
+    date_application DATE         NOT NULL,
+    valeur           VARCHAR(50)  NOT NULL,
+    PRIMARY KEY (id)
 );
 
 CREATE TABLE tresorerie
 (
-    id             INT AUTO_INCREMENT PRIMARY KEY,
+    id             INT AUTO_INCREMENT,
     date_mouvement DATE           NOT NULL,
-    solde          DECIMAL(15, 2) NOT NULL
+    solde          DECIMAL(15, 2) NOT NULL,
+    PRIMARY KEY (id)
 );
 
 CREATE TABLE compte_client
 (
-    id            INT AUTO_INCREMENT PRIMARY KEY,
+    id            INT AUTO_INCREMENT,
     numero        VARCHAR(50) NOT NULL,
     date_creation DATE,
     client_id     INT         NOT NULL,
-    FOREIGN KEY (client_id) REFERENCES clients (id)
+    PRIMARY KEY (id),
+    FOREIGN KEY (client_id) REFERENCES client (id)
 );
 
 CREATE TABLE mouvement_solde
 (
-    id               INT PRIMARY KEY AUTO_INCREMENT,
+    id               INT AUTO_INCREMENT,
     montant          DECIMAL(15, 2) NOT NULL,
     date_mouvement   DATE,
     compte_client_id INT            NOT NULL,
+    PRIMARY KEY (id),
     FOREIGN KEY (compte_client_id) REFERENCES compte_client (id)
 );
 
 CREATE TABLE enum_status_compte
 (
-    id      INT AUTO_INCREMENT PRIMARY KEY,
-    libelle VARCHAR(255) NOT NULL
-);
-
-CREATE TABLE type_pret
-(
-    id      INT AUTO_INCREMENT PRIMARY KEY,
-    libelle VARCHAR(50)    NOT NULL,
-    taux    DECIMAL(15, 2) NOT NULL
-);
-
-CREATE TABLE paiements
-(
-    id            INT AUTO_INCREMENT PRIMARY KEY,
-    date_paiement DATE           NOT NULL,
-    montant       DECIMAL(15, 2) NOT NULL,
-    client_id     INT            NOT NULL,
-    employees_id  INT,
-    FOREIGN KEY (client_id) REFERENCES clients (id),
-    FOREIGN KEY (employees_id) REFERENCES employees (id)
-);
-
-CREATE TABLE paiement_modalite
-(
-    id              INT AUTO_INCREMENT PRIMARY KEY,
-    date_retour     DATE           NOT NULL,
-    numero_paiement INTEGER,
-    montant_a_payer DECIMAL(15, 2) NOT NULL,
-    paiement_id     INT            NOT NULL,
-    FOREIGN KEY (paiement_id) REFERENCES paiements (id)
-);
-
-CREATE TABLE pret
-(
-    id           INT AUTO_INCREMENT PRIMARY KEY,
-    montant      DECIMAL(15, 2) NOT NULL,
-    type_pret_id INT            NOT NULL,
-    modalite_id  INT            NOT NULL,
-    duree        INT            NOT NULL, -- en mois
-    FOREIGN KEY (modalite_id) REFERENCES modalite (id),
-    FOREIGN KEY (type_pret_id) REFERENCES type_pret (id)
+    id      INT AUTO_INCREMENT,
+    libelle VARCHAR(255) NOT NULL,
+    PRIMARY KEY (id)
 );
 
 CREATE TABLE status_pret
 (
-    enum_pret_id INT,
-    type_pret_id INT,
+    id           INT AUTO_INCREMENT,
     date_status  DATE NOT NULL,
-    PRIMARY KEY (enum_pret_id, type_pret_id),
-    FOREIGN KEY (enum_pret_id) REFERENCES enum_pret (id),
+    employees_id INT  NOT NULL,
+    enum_pret_id INT  NOT NULL,
+    PRIMARY KEY (id),
+    FOREIGN KEY (employees_id) REFERENCES employe (id),
+    FOREIGN KEY (enum_pret_id) REFERENCES enum_status_pret (id)
+);
+
+CREATE TABLE pret
+(
+    id                  INT AUTO_INCREMENT,
+    duree_remboursement DECIMAL(15, 2) NOT NULL,
+    montant             DECIMAL(15, 2) NOT NULL,
+    date_demande        INT            NOT NULL,
+    modalite_id         INT            NOT NULL,
+    employees_id        INT            NOT NULL,
+    compte_client_id    INT            NOT NULL,
+    type_pret_id        INT            NOT NULL,
+    PRIMARY KEY (id),
+    FOREIGN KEY (modalite_id) REFERENCES modalite (id),
+    FOREIGN KEY (employees_id) REFERENCES employe (id),
+    FOREIGN KEY (compte_client_id) REFERENCES compte_client (id),
     FOREIGN KEY (type_pret_id) REFERENCES type_pret (id)
+);
+
+CREATE TABLE paiement
+(
+    id            INT AUTO_INCREMENT,
+    date_paiement DATE           NOT NULL,
+    montant       DECIMAL(15, 2) NOT NULL,
+    pret_id       INT            NOT NULL,
+    employees_id  INT            NOT NULL,
+    PRIMARY KEY (id),
+    FOREIGN KEY (pret_id) REFERENCES pret (id),
+    FOREIGN KEY (employees_id) REFERENCES employe (id)
+);
+
+CREATE TABLE paiement_modalite
+(
+    id                 INT AUTO_INCREMENT,
+    date_prevu_paiment DATE           NOT NULL,
+    montant_prevu      DECIMAL(15, 2) NOT NULL,
+    pret_id            INT            NOT NULL,
+    PRIMARY KEY (id),
+    FOREIGN KEY (pret_id) REFERENCES pret (id)
 );
 
 CREATE TABLE status_client
