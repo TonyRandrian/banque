c2acd86be0f89acf5204daa19159faaa555d9da8
TonyRandrian <tony.randrian855@gmail.com>
2025-07-08 08:32:11 +0300

    data sql

--- ws/controllers/PretController.php
+++ ws/controllers/PretController.php
@@ -185,11 +185,13 @@ class PretController
         $data = Flight::request()->data;
         $date_debut = isset($data['date_debut']) ? $data['date_debut'] : null;
         $date_fin = isset($data['date_fin']) ? $data['date_fin'] : null;
+
         if (!$date_debut || !$date_fin) {
             http_response_code(400);
             echo json_encode(["error" => "date_debut et date_fin requis"]);
             return;
         }
+
         require_once __DIR__ . '/../models/Pret.php';
         $result = Pret::getInteretsParMoisTousPrets($date_debut, $date_fin);
         echo json_encode($result);
@@ -204,11 +206,13 @@ class PretController
         $data = Flight::request()->data;
         $date_debut = isset($data['date_debut']) ? $data['date_debut'] : null;
         $date_fin = isset($data['date_fin']) ? $data['date_fin'] : null;
+
         if (!$date_debut || !$date_fin) {
             http_response_code(400);
             echo json_encode(["error" => "date_debut et date_fin requis"]);
             return;
         }
+
         require_once __DIR__ . '/../models/Pret.php';
         $result = Pret::getInteretsParMoisPourPret($pret_id, $date_debut, $date_fin);
         echo json_encode($result);
